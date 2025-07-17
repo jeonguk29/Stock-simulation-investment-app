abf232b9e9d4525018613d3abdcc09195a8d7289
정정욱 <jeonguk29@naver.com>
2025-07-17 20:38:11 +0900

    feat: 사용자 티커 검색 API를 SearchViewModel에 연결

--- TradeUp/TradeUp/ViewModels/SearchViewModel.swift
+++ TradeUp/TradeUp/ViewModels/SearchViewModel.swift
@@ -11,7 +11,7 @@ import StocksAPI
 
 @MainActor
 class SearchViewModel: ObservableObject {
-
+    
     @Published var query: String = ""
     @Published var phase: FetchPhase<[Ticker]> = .initial
     
@@ -28,10 +28,45 @@ class SearchViewModel: ObservableObject {
     }
     
     private var cancellables = Set<AnyCancellable>()
-    private let stocksAPI: StocksAPI
+    private let stocksAPI: StockRepository // 프로토콜화
     
-    init(query: String = "", stocksAPI: StocksAPI = StocksAPI()) {
+    init(query: String = "", stocksAPI: StockRepository = StocksAPI()) {
         self.query = query
         self.stocksAPI = stocksAPI
+        startObserving()
+     }
+     
+     private func startObserving() {
+         $query // 사용자 입역 쿼리를 구독해서 변경이 생기는것을 관찰
+             .debounce(for: 0.25, scheduler: DispatchQueue.main)
+             .sink { _ in
+                 Task { [weak self] in await self?.searchTickers() }
+             }
+             .store(in: &cancellables)
+         
+         $query
+             .filter { $0.isEmpty }
+             .sink { [weak self] _ in self?.phase = .initial }
+             .store(in: &cancellables)
+     }
+    
+    func searchTickers() async {
+        let searchQuery = trimmedQuery
+        guard !searchQuery.isEmpty else { return }
+        phase = .fetching
+        
+        do {
+            let tickers = try await stocksAPI.searchTickers(query: searchQuery, isEquityTypeOnly: true)
+            if searchQuery != trimmedQuery { return }
+            if tickers.isEmpty {
+                phase = .empty
+            } else {
+                phase = .success(tickers)
+            }
+        } catch {
+            if searchQuery != trimmedQuery { return }
+            print(error.localizedDescription)
+            phase = .failure(error)
+        }
     }
 }
